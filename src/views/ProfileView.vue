8570a7d60786efe6939f7564c2afd5e16804bd27
pussik <vlad.hobta79@mail.ru>
2022-08-15 22:48:08 +0300

    Working with vue login and singup

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,3 +1,20 @@
 <template>
-    <h3>Profile</h3>
+  <div class="container-lg">
+    <h3 class="ms-3">Profile</h3>
+    <h4 class="ms-3">Some Info about profile:</h4>
+  </div>
 </template>
+
+<script>
+
+export default {
+    name: "ProfileView",
+    data() {
+    return {
+        userData: null,
+    };
+
+    
+
+}
+</script>
